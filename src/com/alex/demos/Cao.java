9848e06c8d04b92a9f2e460770859c8dbb063c4a
AlexLiu1985CN <AlexLiu1985CN@gmail.com>
2015-01-08 16:33:21 +0800

    4

--- src/com/alex/demos/Cao.java
+++ src/com/alex/demos/Cao.java
@@ -4,10 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Cao extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		caonijuhua();
 	}
-	
+
+	private String caonijuhua() {
+		String aa = "caonide";
+		String ss = "juhua";
+		String zz = aa + ss;
+		return zz;
+	}
 }
