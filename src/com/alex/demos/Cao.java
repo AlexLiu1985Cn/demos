5c401b54b27369eba12681d7dfc0853c1e0b083d
AlexLiu1985CN <AlexLiu1985CN@gmail.com>
2015-01-08 16:06:39 +0800

    add cao

--- src/com/alex/demos/Cao.java
+++ src/com/alex/demos/Cao.java
@@ -1,7 +1,13 @@
 package com.alex.demos;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class Cao extends Activity {
-
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
 }
